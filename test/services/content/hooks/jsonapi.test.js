e77d5a97d0c0610445a49365377d56d870f32d14
Sebastian Kliem <sebastian-kliem@live.de>
2016-11-30 12:07:52 +0100

    tests: fix broken path for loading a module

--- test/services/content/hooks/jsonapi.test.js
+++ test/services/content/hooks/jsonapi.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const jsonapi = require('../../../../src\services\content\hooks\jsonapi.js');
+const jsonapi = require('../../../../src/services/content/hooks/jsonapi.js');
 
 describe('content jsonapi hook', function() {
   it('hook can be used', function() {
