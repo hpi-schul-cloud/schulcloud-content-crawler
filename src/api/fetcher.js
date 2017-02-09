c327b2260cb4ad70724b47347d8262ccf115f4f7
Dominic Sauer <dominic.sauer@yahoo.de>
2017-02-09 12:55:17 +0100

    Change update procedure: Upsert instead of removing all entities and insert new ones

--- src/api/fetcher.js
+++ src/api/fetcher.js
@@ -55,21 +55,31 @@ function fetchData (excludedClients) {
 
 function insertIntoDatabase(clientName, data) {
   console.log(clientName + ': fetched ' + data.length + ' entities');
-  data.forEach((entity) => {
-    entity.updatedAt = Date.now();
-    entity._id = md5(clientName + '_' + entity.originId).slice(4,28);
-    entity.id = md5(clientName + '_' + entity.originId).slice(4,28);
-    entity.type = "contents";   //needed for jsonapi-server
-    entity.client = clientName;
+
+  let findPromise = contentModel.find({client: clientName}, {_id: true}).exec();
+  return findPromise.then(result => {
+      let ids = result.map(entity => entity._id);
+      console.log(clientName + ': found ' + ids.length + ' existing entities');
+
+      data.forEach(entity => {
+          entity.updatedAt = Date.now();
+          entity._id = md5(clientName + '_' + entity.originId).slice(4, 28);
+          entity.id = md5(clientName + '_' + entity.originId).slice(4, 28);
+          entity.type = "contents";   //needed for jsonapi-server
+          entity.client = clientName;
+          let index = ids.indexOf(entity._id);
+          if (index > -1) {
+              ids.splice(index, 1);
+          }
+      });
+
+      let removePromise = contentModel.remove({_id: {$in: ids}}).exec();
+      return removePromise
+          .then(result => console.log(clientName + ': deleted ' + result.result.n + ' rows'))
+          .then(() => Promise.all(data.map(entity => contentModel.update({_id: entity._id}, entity, {upsert: true}).exec())))
+          .then(result => {
+              console.log(clientName + ': inserted ' + result.filter(row => row.upserted !== undefined).length + ' rows');
+              console.log(clientName + ': modified ' + result.filter(row => (row.upserted === undefined) && (row.nModified == 1)).length + ' rows');
+          });
   });
-
-  let removePromise = contentModel.remove({client: clientName}).exec();
-
-  return removePromise.then(
-      (result) => console.log(clientName + ': deleted ' + result.result.n + ' rows in db')
-    ).then(
-      () => contentModel.collection.insert(data, { ordered: false })
-    ).then(
-      (result) => console.log(clientName + ': inserted ' + result.insertedCount + ' rows to db')
-    );
 }
