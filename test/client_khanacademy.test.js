a3537ec28bd96b84d969daff1fbf5b788e971d28
Sebastian Kliem <sebastian-kliem@live.de>
2017-02-21 23:49:06 +0100

    [WIP] add test for Khan Client

--- test/client_khanacademy.test.js
+++ test/client_khanacademy.test.js
@@ -4,19 +4,46 @@ let request = require('request-promise');
 let sinon = require('sinon');
 require('sinon-as-promised');
 let khanClient = require('../src/clients/khanacademy');
+let khanResponse = require('./khanResponse.json');
+let correctContentModel = {
+    originId: 'x3f2a1471',
+    title: 'Counting small numbers',
+    url: 'https://de.khanacademy.org/math/early-math/cc-early-math-counting-topic/cc-early-math-counting',
+    license: null,
+    description: 'Learn to count from 0 to 20.',
+    contentType: 50,
+    creationDate: null,
+    lastModified: new Date("2016-09-13T20:36:00.000Z"),
+    subjects: ['380'],
+    tags: ['Einstieg in die Mathematik', 'Das Zählen'],
+    restrictions: null,
+    relatedResources: [{
+            relationType: 'episode',
+            originId: 'x9b4a5e7a'
+        },
+        {
+            relationType: 'episode',
+            originId: 'x1a52b63e'
+        }
+    ],
+    popularity: 0,
+    editorsPick: false
+};
 
 
 describe('khan client', function() {
-  describe('getAll', function() {
-    before(function() {
-      sinon.stub(request, 'get').resolves(JSON.stringify({test: 'val'}));
-    });
+    describe('getAll', function() {
+        before(function() {
+            sinon.stub(request, 'get').resolves(JSON.stringify(khanResponse));
+        });
 
-    after(function() {
-      request.get.restore();
+        after(function() {
+            request.get.restore();
+        });
+        it('converts the JSON response correctly to a contentModel', function() {
+            return khanClient.getAll().then(function(result) {
+              assert.equal(JSON.stringify(result[0]), JSON.stringify(correctContentModel));
+            });
+        });
     });
-    it('converts the JSON response correctly to a contentModel', function() {
-      khanClient.getAll();
-    });
-  });
 });
